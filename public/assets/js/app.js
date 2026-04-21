99cc8283cc4ffe5fde335ae019b54d3e6523edb9
Andrew Johnson <andrewjohnson11235@gmail.com>
2026-04-20 21:11:58 -0500

    feat: add side bar and input locations for jobs, education, and projects

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,6 +19,51 @@ function updateUI() {
     }
 }
 
+
+let currentTab = 'jobs'
+function switchTab(tab) {
+    currentTab = tab
+    const title = document.getElementById('view-title')
+    const formContainer = document.getElementById('divDynamicFormFields')
+
+    // clear previous editor content
+    quill.setContents([])
+
+    if (tab === 'jobs') {
+        title.innerText = "Work Experience"
+        formContainer.innerHTML = `
+            <div class="row g-3">
+                <div class="col-md-6"><label class="form-label">Company</label><input type="text" id="jobCompany" class="form-control" placeholder="e.g. Google"></div>
+                <div class="col-md-6"><label class="form-label">Role</label><input type="text" id="jobRole" class="form-control" placeholder="e.g. Software Engineer"></div>
+                <div class="col-md-6"><label class="form-label">Location</label><input type="text" id="jobLocation" class="form-control" placeholder="City, State"></div>
+                <div class="col-md-6"><label class="form-label">Dates</label><input type="text" id="jobDate" class="form-control" placeholder="Month Year - Month Year"></div>
+            </div>`
+    } else if (tab === 'education') {
+        title.innerText = "Education History"
+        formContainer.innerHTML = `
+            <div class="row g-3">
+                <div class="col-md-6"><label class="form-label">School</label><input type="text" id="eduSchool" class="form-control" placeholder="University Name"></div>
+                <div class="col-md-6"><label class="form-label">Degree/Major</label><input type="text" id="eduDegree" class="form-control" placeholder="e.g. B.S. Computer Science"></div>
+                <div class="col-md-6"><label class="form-label">GPA</label><input type="text" id="eduGpa" class="form-control" placeholder="0.00"></div>
+                <div class="col-md-6"><label class="form-label">Dates</label><input type="text" id="eduDate" class="form-control" placeholder="Month Year - Month Year"></div>
+            </div>`
+    } else if (tab === 'projects') {
+        title.innerText = "Technical Projects"
+        formContainer.innerHTML = `
+            <div class="row g-3">
+                <div class="col-md-12"><label class="form-label">Project Title</label><input type="text" id="projTitle" class="form-control" placeholder="Project Name"></div>
+                <div class="col-md-6"><label class="form-label">GitHub/Demo Link</label><input type="text" id="projLink" class="form-control" placeholder="https://github.com/..."></div>
+                <div class="col-md-6"><label class="form-label">Tech Stack</label><input type="text" id="projStack" class="form-control" placeholder="e.g., Node.js, ChartJS, SQLite"></div>
+            </div>`
+    }
+
+    // auto close sidebar on mobile
+    const sidebar = document.getElementById('vaultSidebar')
+    const instance = bootstrap.Offcanvas.getInstance(sidebar)
+    if (instance) instance.hide()
+}
+
+
 // --- CLEAR AUTH FIELDS ---
 function clearAuthFields() {
     document.getElementById('txtEmail').value = ''
